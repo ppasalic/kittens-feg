c74827c10866a6ea9b0390773c0907c2578cea4c
ppasalic <paola.pasalic@tommy.hr>
2024-07-19 00:52:25 +0200

    Add more json data, implement filter by, rename component name

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Kitten List</h1>
     <KittensSortBy @sort-by="handleOnSortChange" />
+    <KittensFilterBy @filter-by="handleOnFilterCheck" />
     <div class="kitten-list">
       <KittenCard
         v-for="kitten in visibleKittens"
@@ -19,7 +20,8 @@
 <script lang="ts">
 import { ref, onMounted, defineComponent, watch } from 'vue';
 import KittenCard from '../components/KittenCard.vue';
-import KittensSortBy from '../components/KittenSortBy.vue';
+import KittensSortBy from '../components/KittensSortBy.vue';
+import KittensFilterBy from '../components/KittensFilterBy.vue';
 
 interface Kitten {
   id: number;
@@ -34,18 +36,25 @@ interface SortOptions {
   sortOrder: 'asc' | 'desc';
 }
 
+interface FilterOptions {
+  filters: string[];
+}
+
 export default defineComponent({
   components: {
     KittenCard,
-    KittensSortBy
+    KittensSortBy,
+    KittensFilterBy
   },
   setup() {
     const kittens = ref<Kitten[]>([]);
+    const filteredKittens = ref<Kitten[]>([]);
     const visibleKittens = ref<Kitten[]>([]);
     const showButton = ref<boolean>(false);
     const sortOptions = ref<SortOptions>({ sortCriteria: 'age', sortOrder: 'asc' });
     const showMoreCards = ref<number>(20);
     const isSortResetting = ref<boolean>(false);
+    const filterOptions = ref<FilterOptions>({ filters: [] });
 
     onMounted(async () => {
       try {
@@ -67,23 +76,18 @@ export default defineComponent({
             }
           })
         );
-        sortKittens();
-        updateVisibleKittens();
+        applyFiltersAndSorting();
       } catch (e) {
         console.error('Failed to fetch kittens data:', e);
       }
     });
 
     const updateVisibleKittens = (reset: boolean = false) => {
-      console.log('updateVisibleKittens');
-      const currentLength = visibleKittens.value.length;
-      visibleKittens.value = reset
-        ? kittens.value.slice(0, currentLength)
-        : kittens.value.slice(0, currentLength + showMoreCards.value);
+      const currentLength = reset ? 0 : visibleKittens.value.length;
+      visibleKittens.value = filteredKittens.value.slice(0, currentLength + showMoreCards.value);
+      showButton.value = visibleKittens.value.length < filteredKittens.value.length;
 
-      showButton.value = visibleKittens.value.length < kittens.value.length;
-
-      if (!showButton.value) {
+      if (showButton.value === false) {
         isSortResetting.value = true;
         sortOptions.value.sortCriteria = 'age';
         sortOptions.value.sortOrder = 'asc';
@@ -92,8 +96,7 @@ export default defineComponent({
 
     const sortKittens = () => {
       const { sortCriteria, sortOrder } = sortOptions.value;
-
-      kittens.value.sort((a, b) => {
+      filteredKittens.value.sort((a, b) => {
         if (sortCriteria === 'name') {
           return sortOrder === 'asc' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name);
         } else {
@@ -102,7 +105,59 @@ export default defineComponent({
             : parseInt(b.age) - parseInt(a.age);
         }
       });
+      updateVisibleKittens(true);
+    };
 
+    const filterKittens = () => {
+      if (filterOptions.value.filters.length > 0) {
+        filteredKittens.value = kittens.value.filter((kitten: Kitten) => {
+          const ageInMonths = parseInt(kitten.age.split(' ')[0]);
+          const meetsAgeCriteria =
+            (filterOptions.value.filters.includes('youngerThan6Months') && ageInMonths < 6) ||
+            (filterOptions.value.filters.includes('youngerThan12Months') && ageInMonths < 12);
+          const meetsColorCriteria =
+            filterOptions.value.filters.includes('blackColor') && kitten.color === 'Black';
+
+          if (filterOptions.value.filters.length === 0) {
+            return true;
+          }
+
+          if (
+            filterOptions.value.filters.includes('youngerThan6Months') &&
+            filterOptions.value.filters.includes('blackColor')
+          ) {
+            return ageInMonths < 6 && kitten.color === 'Black';
+          }
+
+          if (
+            filterOptions.value.filters.includes('youngerThan12Months') &&
+            filterOptions.value.filters.includes('blackColor')
+          ) {
+            return ageInMonths < 12 && kitten.color === 'Black';
+          }
+
+          if (filterOptions.value.filters.includes('youngerThan6Months')) {
+            return ageInMonths < 6;
+          }
+
+          if (filterOptions.value.filters.includes('youngerThan12Months')) {
+            return ageInMonths < 12;
+          }
+
+          if (filterOptions.value.filters.includes('blackColor')) {
+            return kitten.color === 'Black';
+          }
+
+          return meetsAgeCriteria || meetsColorCriteria;
+        });
+      } else {
+        filteredKittens.value = [...kittens.value];
+      }
+    };
+
+    const applyFiltersAndSorting = () => {
+      filterKittens();
+      sortKittens();
       updateVisibleKittens(true);
     };
 
@@ -116,22 +171,33 @@ export default defineComponent({
         return;
       }
       sortOptions.value = filters;
+      applyFiltersAndSorting();
+    };
+
+    const handleOnFilterCheck = (filters: FilterOptions) => {
+      filterOptions.value = filters;
+      applyFiltersAndSorting();
     };
 
     watch(
-      sortOptions,
+      [sortOptions, filterOptions],
       () => {
-        sortKittens();
+        applyFiltersAndSorting();
       },
       { deep: true }
     );
 
-    return { visibleKittens, showButton, handleOnShowMoreClick, handleOnSortChange };
+    return {
+      visibleKittens,
+      showButton,
+      handleOnShowMoreClick,
+      handleOnSortChange,
+      handleOnFilterCheck
+    };
   }
 });
 </script>
 
 <style lang="scss">
-// the main file that imports everything related with styles
 @import '@/assets/scss/homeView.scss';
 </style>
