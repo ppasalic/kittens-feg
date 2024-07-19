66ec1af42a9cfd7107dd6328ad1824b4ed6d3757
ppasalic <paolapasalic94@gmail.hr>
2024-07-20 00:21:38 +0200

    Style KittenFilterby

--- src/components/KittensFilterBy.vue
+++ src/components/KittensFilterBy.vue
@@ -1,29 +1,35 @@
 <template>
   <div class="filter-by-container">
-    <input
-      type="checkbox"
-      id="youngerThan6Months"
-      value="youngerThan6Months"
-      v-model="filters"
-      @change="handleOnFilterCheck"
-    />
-    <label for="youngerThan6Months"> Younger than 6 months </label>
-    <input
-      type="checkbox"
-      id="youngerThan12Months"
-      value="youngerThan12Months"
-      v-model="filters"
-      @change="handleOnFilterCheck"
-    />
-    <label for="youngerThan12Months"> Younger than 12 months </label>
-    <input
-      type="checkbox"
-      id="blackColor"
-      value="blackColor"
-      v-model="filters"
-      @change="handleOnFilterCheck"
-    />
-    <label for="blackColor"> Black color </label>
+    <div class="filter-by-checkbox">
+      <input
+        type="checkbox"
+        id="youngerThan6Months"
+        value="youngerThan6Months"
+        v-model="filters"
+        @change="handleOnFilterCheck"
+      />
+      <label for="youngerThan6Months"> Younger than 6 months </label>
+    </div>
+    <div class="filter-by-checkbox">
+      <input
+        type="checkbox"
+        id="youngerThan12Months"
+        value="youngerThan12Months"
+        v-model="filters"
+        @change="handleOnFilterCheck"
+      />
+      <label for="youngerThan12Months"> Younger than 12 months </label>
+    </div>
+    <div class="filter-by-checkbox">
+      <input
+        type="checkbox"
+        id="blackColor"
+        value="blackColor"
+        v-model="filters"
+        @change="handleOnFilterCheck"
+      />
+      <label for="blackColor"> Black color </label>
+    </div>
   </div>
 </template>
 
@@ -52,5 +58,5 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-@import '@/assets/scss/kittenSortBy.scss';
+@import '@/assets/scss/kittenFilterBy.scss';
 </style>
